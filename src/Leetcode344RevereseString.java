a6034a9a4d39f96ca7512a8de0b27db6b026ed47
Sanjay Ramalingam <sanjayramaligam777@gmail.com>
2025-04-02 09:58:43 +0530

    -m

---
+++ src/Leetcode344RevereseString.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+
+//https://leetcode.com/problems/reverse-string/description/
+public class Leetcode344RevereseString {
+    public static String reverseString(char[] s)
+    {
+        StringBuffer sc = new StringBuffer();
+        for(char c:s)
+            sc.append(c);
+        //    sc.reverse();
+        char [] rv = new char[s.length];
+        int len=s.length-1;
+        int i=0;
+        for(int j=len;j>=0;j--)
+        {
+            rv[i]=s[j];
+            //   System.out.println(rv[i]);
+            i++;
+        }
+
+
+
+        return sc.reverse().toString();
+        //return rv.;
+
+    }
+public static void main(String[] args) {
+
+    char[] s = {'h', 'e', 'l', 'l', 'o'};
+    System.out.println (reverseString (s));
+}}
